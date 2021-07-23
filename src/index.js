8e0c7e3f05451dd70f9ffbdc825d479965854e55
alexbelloni <alexbelloni@outlook.com>
2021-07-23 16:14:40 -0400

    running

--- src/index.js
+++ src/index.js
@@ -2,21 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
-window.renderCats = (containerId, history) => {
+window.renderDogs = (containerId, history) => {
   ReactDOM.render(
     <App history={history} />,
     document.getElementById(containerId),
   );
-  serviceWorker.unregister();
 };
 
-window.unmountCats = containerId => {
+window.unmountDogs = containerId => {
   ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
 };
 
 if (!document.getElementById('Dogs-container')) {
   ReactDOM.render(<App />, document.getElementById('root'));
-  serviceWorker.unregister();
 }
